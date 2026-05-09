665dc744d22e5a75c138e79bc79a519186828dce
chunker <github.diving763@passmail.net>
2026-05-09 10:53:35 +1000

    Add project files.

---
+++ DisplayOrbsCode/Placeholder.cs
@@ -0,0 +1,7 @@
+﻿namespace DisplayOrbs.DisplayOrbsCode
+{
+    public class Placeholder
+    {
+        //You're recommended but not required to keep all your code in this package and all your assets in the DisplayOrbs folder.
+    }
+}
