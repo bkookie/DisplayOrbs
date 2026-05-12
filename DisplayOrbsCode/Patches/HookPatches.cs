6202fee61c122a3cb33447959c97dbfcd8cc0b1f
chunker <github.diving763@passmail.net>
2026-05-12 21:01:32 +1000

    add patch for AfterOrbChanneled to push orb to front

---
+++ DisplayOrbsCode/Patches/HookPatches.cs
@@ -0,0 +1,20 @@
+﻿using DisplayOrbs.DisplayOrbsCode.Orbs;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Hooks;
+using MegaCrit.Sts2.Core.Models;
+
+namespace DisplayOrbs.DisplayOrbsCode.Patches;
+
+[HarmonyPatch(typeof(Hook), nameof(Hook.AfterOrbChanneled))]
+public static class AfterOrbChannelledPatch
+{
+    [HarmonyPrefix]
+    private static void AfterOrbChannelled(Player player, OrbModel orb)
+    {
+        if (orb is not DisplayOrbModel)
+        {
+            DisplayOrbManager.PushRealOrbToFront(player, orb);
+        }
+    }
+}
